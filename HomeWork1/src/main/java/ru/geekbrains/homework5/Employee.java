9606c6dbbedaf7f472d2d3d1221f87d5318f7f74
anemchenko <anemchenko@nes.ru>
2020-09-12 22:55:28 +0300

    Hometask5 + Employee class

--- HomeWork1/src/main/java/ru/geekbrains/homework5/Employee.java
+++ HomeWork1/src/main/java/ru/geekbrains/homework5/Employee.java
@@ -1,4 +1,34 @@
 package ru.geekbrains.homework5;
 
-public class employee {
+public class Employee {
+    private String FIO;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    void getInfo(){
+        System.out.println("FIO - "+ this.FIO);
+        System.out.println("position - "+ this.position);
+        System.out.println("email - "+ this.email);
+        System.out.println("phone - "+ this.phone);
+        System.out.println("salary - "+ this.salary);
+        System.out.println("age - "+ this.age);
+    }
+    public Employee(){
+        System.out.println("Объект класса 'Employee' создан, но поля не инициализированы.");
+
+    }
+    Employee(String FIO, String position, String email, String phone, int salary, int age){
+        this.FIO = FIO;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+    int getAge(){
+        return (this.age);
+    }
 }
--- HomeWork1/src/main/java/ru/geekbrains/homework5/HomeWork5.java
+++ HomeWork1/src/main/java/ru/geekbrains/homework5/HomeWork5.java
@@ -1,4 +1,18 @@
 package ru.geekbrains.homework5;
 
 public class HomeWork5 {
+    public static void main(String[] args) {
+        Employee[] persArray = new Employee[5];
+        persArray[0] = new Employee("Nemchenko Anton Valerievich","System Analyst", "anemchenko@nes.ru", "89295494066", 103000,26);
+        persArray[1] = new Employee("Monahova Alina Olegovna","System Analyst", "alina@mail.ru", "89231234567", 50000,25);
+        persArray[2] = new Employee("Scherbakov Oleg Olegovich","Engineer", "oleg@mail.ru", "89212345678", 56000,30);
+        persArray[3] = new Employee("Savin Aleksandr Kirillovich","Junior Developer", "sasha@mail.ru", "89223456789", 30000,20);
+        persArray[4] = new Employee("Obidin Andrey Viktorovich ","Head of IT Department", "andrey@mail.ru", "89245678912", 200000,41);
+
+        for (Employee employee : persArray) {
+            if (employee.getAge() > 40) {
+                employee.getInfo();
+            }
+        }
+    }
 }
